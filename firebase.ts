1f3add3fb0fbd1046df99139e09b6a8da13201d4
isihatcustserviceshasha-wq <isihatcustserviceshasha@gmail.com>
2026-03-23 14:59:52 +0800

    Add files via upload

--- firebase.ts
+++ firebase.ts
@@ -1,8 +1,10 @@
-import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
-import firebaseConfig from '../firebase-applet-config.json';
+import {StrictMode} from 'react';
+import {createRoot} from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
 
-const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app, firebaseConfig.firestoreDatabaseId);
-export const auth = getAuth(app);
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+);
